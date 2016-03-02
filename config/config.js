1b70db7157b236d42a97e3cd28673f93f9c029fc
aeadigt <aea@digt.ru>
2016-03-02 15:29:39 +0300

    Правки

--- config/config.js
+++ config/config.js
@@ -10,6 +10,7 @@
 	    "username": "user_XXXXXXXXXX"
 	}
     ],
+    "webAuth":"disable",
     "trustedNet":{
         "tokenURL":"https://net.trusted.ru/idp/sso/oauth/token",
         "profileURL": "https://net.trusted.ru/trustedapp/rest/person/profile/get",
