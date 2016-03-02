1d4e1ffe44c27d330b9964e2b7ee32bb27cfd781
aeadigt <aea@digt.ru>
2016-03-02 16:08:49 +0300

    Внесены мелкие правки

--- init.js
+++ init.js
@@ -3,5 +3,5 @@ var mars = require('mars');
 mars.events.on('startWebServer', function (obj) {
     if (obj.port) {
         window.location.href = "http://localhost:" + obj.port;
-a    }
+    }
 });
