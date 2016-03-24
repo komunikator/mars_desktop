bb069297e5ac5630fe16f7c292859bfa7accb851
aeadigt <aea@digt.ru>
2016-03-24 17:17:57 +0300

    Внесены правки

--- init.js
+++ init.js
@@ -4,6 +4,11 @@ new (require('./lib/pick_up.js'))(mars);
 
 mars.events.on('startWebServer', function (obj) {
     if (obj.port) {
-        window.location.href = "http://localhost:" + obj.port;
+        //window.location.href = "http://localhost:" + obj.port;
     }
 });
+
+window.appExtension = mars.events;//for gui.js
+window.sip = mars.sip;//for gui.js
+
+new (require('./lib/softphone.js'))(mars);
