3a2bcdcbcb9b3bf75457d2f923660eb453fd2411
aeadigt <aea@digt.ru>
2016-03-28 17:03:46 +0300

    Добавлен preloader

--- init.js
+++ init.js
@@ -1,17 +1,18 @@
-var mars = require('mars');
+try {
+    var mars = require('mars');
 
-new (require('./lib/pick_up.js'))(mars);
+    mars.events.on('startWebServer', function (obj) {
+        if (obj.port) {
+            ifrm = document.createElement("iframe");
+            ifrm.setAttribute("src", "http://localhost:" + obj.port);
+            document.body.appendChild(ifrm);
 
-mars.events.on('startWebServer', function (obj) {
-    if (obj.port) {
-        ifrm = document.createElement("iframe");
-        ifrm.setAttribute("src", "http://localhost:" + obj.port);
-        document.body.appendChild(ifrm);
-        //window.location.href = "http://localhost:" + obj.port;
-    }
-});
-
-window.appExtension = mars.events;//for gui.js
-window.sip = mars.sip;//for gui.js
-
-new (require('./lib/softphone.js'))(mars);
+            ifrm.addEventListener('load', function () {
+                document.getElementById("preloader").style.display = "none";
+            });
+            //window.location.href = "http://localhost:" + obj.port;
+        }
+    });
+} catch (err) {
+    console.log(err);
+}
